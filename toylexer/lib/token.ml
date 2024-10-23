8d5a252f1adc8dd32af6dc807077b6e20658e9b9
Docker <docker@example.com>
2024-10-23 15:41:00 +0100

    Esercizio toylexer

--- toylexer/lib/token.ml
+++ toylexer/lib/token.ml
@@ -4,9 +4,17 @@ type token =
   | ASSIGN
   | PLUS
   | SEQ
-  | ID of string
+  | ID of string  (*ID + il tipo di ciò che contiene*)
   | CONST of string
   | EOF
+  | ATOK of string
+  | BTOK of string
+  | CTOK of string
+  | DTOK of string
+  | ETOK of string
+
+
+  (* Aggiungi token task 2*)
 
 let string_of_token = function
   | LPAREN -> "LPAREN"
@@ -17,3 +25,9 @@ let string_of_token = function
   | ID(s) -> "ID(" ^ s ^ ")"
   | CONST(s) -> "CONST(" ^ s ^ ")"
   | EOF -> "EOF"
+  | ATOK(s) -> "ATOK(" ^ s ^ ")"
+  | BTOK(s) -> "BTOK(" ^ s ^ ")"
+  | CTOK(s) -> "CTOK(" ^ s ^ ")"
+  | DTOK(s) -> "DTOK(" ^ s ^ ")"
+  | ETOK(s) -> "ETOK(" ^ s ^ ")"
+
